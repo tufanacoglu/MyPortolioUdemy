10c1096358dd7fa3b1355e6a161e5e3dbb216337
Tufan Acoglu <Tufan Acoglu@TUFAN>
2025-04-15 13:27:20 +0300

    Add project files.

---
+++ MyPortolioUdemy/DAL/Context/MyPortfolioContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPortolioUdemy.DAL.Entities;
+
+namespace MyPortolioUdemy.DAL.Context
+{
+    public class MyPortfolioContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Tufan;initial Catalog=MyPortfolioDb;integrated Security=true;trust server certificate=true");
+        }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<ToDoList> ToDoLists { get; set; }
+    }
+}
